4ad1c266111cc64ab6c90c278330484469406ee3
JoanaSD <joir.bilbao@alumnos.cei.es>
2023-12-20 18:52:17 +0100

    funciones
    he añadido las funciones

--- js/index.js
+++ js/index.js
@@ -7,3 +7,35 @@ let resultado = document.querySelector('#resultado')
 let estadisticas = document.querySelector('#estadisticas')
 let exitosos = localStorage.getItem('numeroUsuario') || 0
 let fallidos = localStorage.getItem('numeroUsuario') || 0
+
+//funciones
+
+function generarNumero() {
+    numeroAleatorio = math.floor(Math.random() * 10)
+}
+
+function comprobarNumero(){
+
+    let valorInput = document.querySelector('#numeroUsuario').value
+    valorInput = parselnt(valorInput)
+
+    if (valorInput === numeroAleatorio) {
+        exitosos++
+        localStorage.setItem('exitosos' , exitosos)
+        resultado.innerHTML = '¡Muy bien!'
+        estadisticas.innerHTML = localStorage.getItem('exitosos', 'fallidos')
+        generarNumero()
+    }else {
+        fallidos++
+        localStorage.setItem('fallidos' , fallidos)
+        resultado.innerHTML = 'Fallaste'
+        estadisticas.innerHTML = localStorage.getItem('exitosos', 'fallidos')
+    }
+}
+
+function limpiarDatos() {
+
+    localStorage.clear()
+    location.reload()
+
+}
