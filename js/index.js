714049fda308d59a89a267f246b69555818a2e18
maricarmenzapataprior <mczapat@alumnos.cei.es>
2023-12-20 18:27:01 +0100

    listo

--- js/index.js
+++ js/index.js
@@ -1,17 +1,45 @@
 //VARIABLES
 let random;
-let button= document.querySelector(`.section__button`);
+let buttonC= document.querySelector(`.section__button`);
+let buttonClean = document.querySelector(`.borrarDatos`)
 let result = document.querySelector(`#resultado`);
 let statistics = document.querySelector(`#estadisticas`);
-let exitoso = localStorage.getItem(`exitosos`)|| `0`;
-let fallidos = localStorage.getItem(`fallidos`) || `0`;
+let exitosos = localStorage.getItem(`exitosos`)|| 0;
+let fallidos = localStorage.getItem(`fallidos`) || 0;
 
 //FUNCIONES
 function numeroAleatorio(){
-    random = Math.round(Math.random()*20);
+    random = Math.round(Math.random()*10);
+    console.log(random);
 }
 
 
 function numeroAñadido(){
-    let numero = document.querySelector(`#numeroUsuario`).value
+    let numero = document.querySelector(`#numeroUsuario`).value;
+
+    if( numero === random){
+        exitosos++;
+        localStorage.setItem(`exitosos`, exitosos);
+        result.innerHTML= `¡OLÉ! gastaste la suerte del mes`;
+        result.classList.add(`correcto`);
+        statistics.innerHTML=`Fallidos:${fallidos}. Exitos:${exitosos}`;
+
+    }else{
+        fallidos++;
+        localStorage.setItem(`fallidos`,fallidos);
+        result.innerHTML=`una pena but lo esperabamos que fallaras`;
+        result.classList.add(`incorrecto`);
+        statistics.innerHTML=`Fallidos:${fallidos}. Exitos:${exitosos}`;
+    }
+
 }
+
+function cleanData(){
+    localStorage.clear();
+    location.reload();
+
+}
+
+//botones
+buttonC.addEventListener(`click`, numeroAñadido);
+buttonClean.addEventListener(`click`, cleanData);
